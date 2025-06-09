5055a8ba85d3bfe6f5c03534f85d4d7fe78f6624
MatildaSundqvist <matildasundqvist@live.se>
2025-06-09 19:42:36 +0200

    put routes in wrong file..

--- src/controllers/transaction-controller.mjs
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const router = express.Router();
-
-router.route('/transactions')
-router.route('/transactions/mine')
-router.route('/info')
-
-export default router;
