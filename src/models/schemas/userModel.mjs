c1963c64df018d83239b6254f0984cbc299b6630
MatildaSundqvist <matildasundqvist@live.se>
2025-06-11 09:54:38 +0200

    encrypted password in userSchema

--- src/models/schemas/userModel.mjs
+++ src/models/schemas/userModel.mjs
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const userSchema = new mongoose.Schema({
     firstName: {
@@ -18,5 +19,12 @@ const userSchema = new mongoose.Schema({
         required: [true, 'Lösenord måste anges'],
     },
 });
+
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+
+    this.password = await bcrypt.hash(this.password, 11)
+    next();
+})
 
 export default mongoose.model('User', userSchema);
