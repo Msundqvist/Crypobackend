f76ec7ec1fcdbbd5dbeb7b8c49016ecee0ce618e
MatildaSundqvist <matildasundqvist@live.se>
2025-06-17 20:49:27 +0200

    added structure for tcreating transactions

---
+++ client/src/createTransactions.js
@@ -0,0 +1,22 @@
+const createTransForm = document.getElementById('form')
+const recipient = document.querySelector('#recipient')
+const amount = document.querySelector('#amount')
+
+const initApp = async () => {
+
+}
+
+
+
+const handleCreateTransaction = async (e) => {
+    e.preventDefault();
+}
+
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', initApp)
+createTransForm.addEventListener('submit', handleCreateTransaction)
