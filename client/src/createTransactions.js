825c2c276a89cd07ac3087819cf519f4840b7417
MatildaSundqvist <matildasundqvist@live.se>
2025-06-18 21:20:55 +0200

    d

--- client/src/createTransactions.js
+++ client/src/createTransactions.js
@@ -1,4 +1,5 @@
-const createTransForm = document.getElementById('form')
+
+const createTransForm = document.querySelector('#transaction')
 const recipient = document.querySelector('#recipient')
 const amount = document.querySelector('#amount')
 
@@ -10,10 +11,30 @@ const initApp = async () => {
 
 const handleCreateTransaction = async (e) => {
     e.preventDefault();
+    try {
+        const transactions = {
+            recipient: recipient.value,
+            amount: amount.value
+
+        }
+        const response = await fetch('http://localhost:3000/api/wallet/transactions', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'Application/json',
+
+            },
+            body: JSON.stringify(transactions)
+        }
+        )
+        if (response.ok) {
+            const result = await response.json()
+            console.log(result)
+        }
+    } catch (error) {
+        console.error('du har som vanligt gjort fel'
+        )
+    }
 }
-
-
-
 
 
 
