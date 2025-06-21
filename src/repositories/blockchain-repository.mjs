b329e5824ef8b4efc4059fbbf20a81bb34fd53c3
MatildaSundqvist <matildasundqvist@live.se>
2025-06-21 17:02:43 +0200

    saved working code

--- src/repositories/blockchain-repository.mjs
+++ src/repositories/blockchain-repository.mjs
@@ -1,7 +1,9 @@
 import blockchain from "../models/schemas/blockchainModel.mjs";
 
 export default class BlockchainRepository {
-
+    async add(block) {
+        return await blockchain.create(block)
+    }
     async list() {
         return await blockchain.find();
     }
